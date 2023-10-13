af47052b14f275942b9e38ae91908483cca0edc1
Henrique Pignatari Moraes Rosa <henrique.pignatarimoraes@gmail.com>
2023-10-13 00:18:27 -0300

    feat: add a basket api with redis

---
+++ Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,11 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set; }
+
+    }
+}
